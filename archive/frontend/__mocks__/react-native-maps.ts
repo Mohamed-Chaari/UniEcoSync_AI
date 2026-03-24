2d93c5d831fef41f763513d422e62d282a7481c6
Mohamed-Chaari <110133221+Mohamed-Chaari@users.noreply.github.com>
2026-03-24 16:18:48 +0100

    Delete archive directory

--- archive/frontend/__mocks__/react-native-maps.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-
-const MapView = (props) => <View testID="mock-map-view" {...props} />;
-const Marker = (props) => <View testID="mock-marker" {...props} />;
-
-export default MapView;
-export { Marker };
